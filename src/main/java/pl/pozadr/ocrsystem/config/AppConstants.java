5ef166bef687705412845e88ff1beb99bd0f816e
Pozadr <adpozor@gmail.com>
2021-02-18 09:56:33 +0100

    AppConstants refactor

--- src/main/java/pl/pozadr/ocrsystem/config/AppConstants.java
+++ src/main/java/pl/pozadr/ocrsystem/config/AppConstants.java
@@ -3,9 +3,14 @@ package pl.pozadr.ocrsystem.config;
 /**
  * Constants for the entire application.
  */
-public class AppConstants {
+public final class AppConstants {
     public static final int MAX_DB_URL_LENGTH = 1000;
     public static final int MAX_DB_CONTENT_LENGTH = 1000;
-    public static final String TESSERACT_DATA_PATH = "./.apt/usr/share/tesseract-ocr/4.00/tessdata";//"/app.apt/usr/bin/tesseract"; //"./tessdata";
+    public static final String TESSERACT_DATA_PATH = "./.apt/usr/share/tesseract-ocr/4.00/tessdata";
+    //"/app.apt/usr/bin/tesseract"; //"./tessdata";
     public static final String UPLOAD_DIR = "./uploads/";
+
+    private AppConstants() {
+
+    }
 }
