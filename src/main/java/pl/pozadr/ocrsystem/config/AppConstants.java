06f9142dc58fa484942db1f5a411d100639e5ee3
Pozadr <adpozor@gmail.com>
2021-02-18 09:58:17 +0100

    AppConstants refactor

--- src/main/java/pl/pozadr/ocrsystem/config/AppConstants.java
+++ src/main/java/pl/pozadr/ocrsystem/config/AppConstants.java
@@ -2,8 +2,13 @@ package pl.pozadr.ocrsystem.config;
 
 /**
  * Constants for the entire application.
+ * Class is final to prevent it from being extended.
+ * It can't be instantiated because of a private constructor.
  */
-public class AppConstants {
+public final class AppConstants {
+    private AppConstants() {
+    }
+
     public static final int MAX_DB_URL_LENGTH = 1000;
     public static final int MAX_DB_CONTENT_LENGTH = 1000;
     public static final String TESSERACT_DATA_PATH = "./tessdata";
